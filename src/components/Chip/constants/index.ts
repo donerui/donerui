220e36972956e6d851d6d64eb8637e0c0e5b4239
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2025-01-01 11:05:39 +0300

    More color variants for Button and Chip components

--- src/components/Chip/constants/index.ts
+++ src/components/Chip/constants/index.ts
@@ -1,40 +1,56 @@
 export const sizeClassnames = {
-  sm: 'text-xs px-2 py-0.5',
-  md: 'text-sm px-3 py-1',
-  lg: 'text-base px-4 py-1.5'
+  sm: 'text-xs px-3 py-1.5',
+  md: 'text-sm px-4 py-2',
+  lg: 'text-base px-5 py-2.5'
 }
 
 export const colorClassnames = {
   primary: {
     solid: 'bg-primary-500 text-white',
-    outline: 'border border-primary-500 text-primary-500'
+    'solid-light': 'bg-primary-100 text-primary-700',
+    outline: 'border border-primary-500 text-primary-500',
+    'outline-fill': 'border border-primary-500 text-primary-500 bg-primary-50'
   },
   secondary: {
     solid: 'bg-secondary-500 text-white',
-    outline: 'border border-secondary-500 text-secondary-500'
+    'solid-light': 'bg-secondary-100 text-secondary-700',
+    outline: 'border border-secondary-500 text-secondary-500',
+    'outline-fill': 'border border-secondary-500 text-secondary-500 bg-secondary-50'
   },
   success: {
     solid: 'bg-green-500 text-white',
-    outline: 'border border-green-500 text-green-500'
+    'solid-light': 'bg-green-100 text-green-700',
+    outline: 'border border-green-500 text-green-500',
+    'outline-fill': 'border border-green-500 text-green-500 bg-green-50'
   },
   danger: {
     solid: 'bg-red-500 text-white',
-    outline: 'border border-red-500 text-red-500'
+    'solid-light': 'bg-red-100 text-red-700',
+    outline: 'border border-red-500 text-red-500',
+    'outline-fill': 'border border-red-500 text-red-500 bg-red-50'
   },
   warning: {
     solid: 'bg-yellow-500 text-white',
-    outline: 'border border-yellow-500 text-yellow-500'
+    'solid-light': 'bg-yellow-100 text-yellow-700',
+    outline: 'border border-yellow-500 text-yellow-500',
+    'outline-fill': 'border border-yellow-500 text-yellow-500 bg-yellow-50'
   },
   info: {
     solid: 'bg-blue-500 text-white',
-    outline: 'border border-blue-500 text-blue-500'
+    'solid-light': 'bg-blue-100 text-blue-700',
+    outline: 'border border-blue-500 text-blue-500',
+    'outline-fill': 'border border-blue-500 text-blue-500 bg-blue-50'
   },
   light: {
     solid: 'bg-neutral-200 text-neutral-800',
-    outline: 'border border-neutral-200 text-neutral-800'
+    'solid-light': 'bg-neutral-100 text-neutral-700',
+    outline: 'border border-neutral-200 text-neutral-800',
+    'outline-fill': 'border border-neutral-200 text-neutral-800 bg-neutral-50'
   },
   dark: {
     solid: 'bg-neutral-800 text-white',
-    outline: 'border border-neutral-800 text-neutral-800'
+    'solid-light': 'bg-neutral-100 text-neutral-700',
+    outline: 'border border-neutral-800 text-neutral-800',
+    'outline-fill': 'border border-neutral-800 text-neutral-800 bg-neutral-50'
   }
 }
--- src/components/Chip/types/index.ts
+++ src/components/Chip/types/index.ts
@@ -1,6 +1,6 @@
 export interface IChipProps extends Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'className'> {
   className?: string | ((props: IChipProps) => string)
-  variant?: 'solid' | 'outline'
+  variant?: 'solid' | 'solid-light' | 'outline' | 'outline-fill'
   color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
   size?: 'sm' | 'md' | 'lg'
 }
